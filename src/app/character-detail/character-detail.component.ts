340dcadd467c5c09e6f097b95644b390ebd632f0
cslaf <cade.schlaefli@gmail.com>
2019-11-19 13:25:51 -0600

    actually fix the removal this time

--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -24,7 +24,6 @@ export class CharacterDetailComponent implements OnInit {
 
   removeCharacter(character: Character) {
     this.characterChanged.emit(character);
-    this.character = null;
   }
 
 }
