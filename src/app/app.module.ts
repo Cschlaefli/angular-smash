2c1e9412f23b89f93a7dbf4ef73ca18d9ad9f6bf
cslaf <cade.schlaefli@gmail.com>
2020-02-19 16:40:41 -0600

    partial migration to ang9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { JwtModule } from '@auth0/angular-jwt';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { JwtModule } from "@auth0/angular-jwt";
 
-import { CharacterProperties } from './character-properties.pipe';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CharacterListComponent } from './character-list/character-list.component';
-import { CharacterDetailComponent } from './character-detail/character-detail.component';
-import { MoveListComponent } from './move-list/move-list.component';
-import { MoveDetailComponent } from './move-detail/move-detail.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule} from '@angular/material/sidenav';
-import { MatGridListModule} from '@angular/material/grid-list';
-import { MatTabsModule} from '@angular/material/tabs';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatListModule } from '@angular/material/list';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCardModule } from '@angular/material/card';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import { SearchPipe } from './search.pipe';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { AboutComponent } from './about/about.component';
-import { RegisterComponent } from './register/register.component';
-import { AlertComponent } from './alert/alert.component';
+import { CharacterProperties } from "./character-properties.pipe";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { HttpClientModule } from "@angular/common/http";
+import { CharacterListComponent } from "./character-list/character-list.component";
+import { CharacterDetailComponent } from "./character-detail/character-detail.component";
+import { MoveListComponent } from "./move-list/move-list.component";
+import { MoveDetailComponent } from "./move-detail/move-detail.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatListModule } from "@angular/material/list";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatCardModule } from "@angular/material/card";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { SearchPipe } from "./search.pipe";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { AboutComponent } from "./about/about.component";
+import { RegisterComponent } from "./register/register.component";
+import { AlertComponent } from "./alert/alert.component";
 
 @NgModule({
   declarations: [
@@ -47,16 +47,10 @@ import { AlertComponent } from './alert/alert.component';
     SearchPipe,
     AboutComponent,
     RegisterComponent,
-    AlertComponent,
+    AlertComponent
   ],
   imports: [
-    JwtModule.forRoot({
-      config: {
-        tokenGetter : function tokenGetter() { 
-          return localStorage.getItem("currentUser");},
-          whitelistedDomains: ['cade.schlaefli.dev']
-        }
-      }),
+    JwtModule,
     NgbModule,
     MatSidenavModule,
     BrowserModule,
@@ -77,9 +71,9 @@ import { AlertComponent } from './alert/alert.component';
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatFormFieldModule,
+    MatFormFieldModule
   ],
-  providers: [ ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
